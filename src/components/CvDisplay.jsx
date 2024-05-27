26d7639a7c82fd8214387b522559b1b480e031a0
DeeBest <simphiwedladla8@gmail.com>
2024-05-27 09:57:50 +0200

    updated the qualification form

--- src/components/CvDisplay.jsx
+++ src/components/CvDisplay.jsx
@@ -6,36 +6,34 @@ import { IoLocationOutline } from 'react-icons/io5';
 const CvDisplay = (props) => {
   return (
     <div className="cv-display">
-      <section className="general-information">
-        <div className="img-container">
-          <img src="./src/assets/images/profile-pic1.jpg" alt="cv image" />
+      <section className="personal-details">
+        <h3>Personal Details</h3>
+        <hr />
+        <h2>{props.personalDetails.name}</h2>
+        <div>
+          <MdOutlineEmail className="personal-details-icons" />
+          <p>{props.personalDetails.email}</p>
         </div>
-        <div className="personal-details">
-          <h3>General Information</h3>
-          <p>{props.personalDetails.name}</p>
-          <div>
-            <MdOutlineEmail className="personal-details-icons" />
-            <p>{props.personalDetails.email}</p>
-          </div>
-          <div>
-            <FiPhone className="personal-details-icons" />
-            <p>{props.personalDetails.phoneNumber}</p>
-          </div>
-          <div>
-            <IoLocationOutline className="personal-details-icons" />
-            <p>{props.personalDetails.location}</p>
-          </div>
+        <div>
+          <FiPhone className="personal-details-icons" />
+          <p>{props.personalDetails.phoneNumber}</p>
+        </div>
+        <div>
+          <IoLocationOutline className="personal-details-icons" />
+          <p>{props.personalDetails.location}</p>
         </div>
       </section>
       <section className="educational-experience">
         <h3>Education</h3>
-        <p>Simphiwe Dladla</p>
-        <p>simphiwedladla8@gmail.com</p>
-        <p>+27732434548</p>
-        <p>Mpumalanga, South Africa</p>
+        <hr />
+        <div className="qualification-details">
+          <h5 className="university-name">University Of Pretoria</h5>
+          <span>08/2014-12/2018</span>
+        </div>
       </section>
       <section className="practical-experience">
         <h3>Practical Experience</h3>
+        <hr />
         <p>Simphiwe Dladla</p>
         <p>simphiwedladla8@gmail.com</p>
         <p>+27732434548</p>
--- src/components/CvForm.jsx
+++ src/components/CvForm.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
+import { IoIosArrowDown } from 'react-icons/io';
+import { FaUserGraduate } from 'react-icons/fa';
+import { FaBriefcase } from 'react-icons/fa6';
+
 const CvForm = (props) => {
   return (
-    <>
-      <form className="cv-form" onSubmit={(e) => e.preventDefault()}>
-        <h3>Personal Information</h3>
+    <div className="cv-form">
+      <h3 className="add-experience">Personal Information</h3>
+      <form onSubmit={(e) => e.preventDefault()}>
         <label htmlFor="name">Name:</label>
         <input
           type="text"
           name="name"
           id="name"
+          value={props.personalDetails.name}
           placeholder="Type your full name..."
           onChange={(e) =>
             props.setPersonalDetails({
@@ -22,6 +27,7 @@ const CvForm = (props) => {
           type="email"
           name="email"
           id="email"
+          value={props.personalDetails.email}
           placeholder="Type your email..."
           onChange={(e) =>
             props.setPersonalDetails({
@@ -35,6 +41,7 @@ const CvForm = (props) => {
           type="tel"
           name="phone-number"
           id="phone-number"
+          value={props.personalDetails.phoneNumber}
           placeholder="Type your phone number..."
           onChange={(e) =>
             props.setPersonalDetails({
@@ -43,11 +50,12 @@ const CvForm = (props) => {
             })
           }
         />
-        <label htmlFor="address">Address:</label>
+        <label htmlFor="address">Location:</label>
         <input
           type="text"
           name="address"
           id="address"
+          value={props.personalDetails.location}
           placeholder="Type your address..."
           onChange={(e) =>
             props.setPersonalDetails({
@@ -57,7 +65,42 @@ const CvForm = (props) => {
           }
         />
       </form>
-    </>
+      <div className="add-experience">
+        <div className="add-experience-header">
+          <FaUserGraduate />
+          <h3>Education</h3>
+          <IoIosArrowDown />
+        </div>
+        <div className="add-experience-body">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="universityName">University:</label>
+            <input
+              type="text"
+              name="universityName"
+              id="universityName"
+              placeholder="University name..."
+            />
+            <label htmlFor="qualification">Qualification:</label>
+            <input
+              type="text"
+              name="qualification"
+              id="qualification"
+              placeholder="Qualification name..."
+            />
+            <label htmlFor="duration">Duration:</label>
+            <input type="text" name="duration" id="duration" />
+            <button className="save-education-btn">Save</button>
+          </form>
+        </div>
+      </div>
+      <div className="add-experience">
+        <div className="add-experience-header">
+          <FaBriefcase />
+          <h3>Practical Experience</h3>
+          <IoIosArrowDown />
+        </div>
+      </div>
+    </div>
   );
 };
 
